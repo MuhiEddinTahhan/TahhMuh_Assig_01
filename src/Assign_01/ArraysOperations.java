462a2caabb01a3de5f0b7473fab171ce5f5b70d3
MuhiEddinTahhan <mdtgonow2@gmail.com>
2022-02-02 03:55:53 -0500

    assignment#1

--- src/Assign_01/ArraysOperations.java
+++ src/Assign_01/ArraysOperations.java
@@ -1,10 +1,23 @@
+/**
+ *Name: Muhi Eddin Tahhan
+ *date: 1/2/2022
+ * discribtion: assignment#1 try to make some operations on arrays
+ */
+
 package Assign_01;
 
+/**
+ * main class
+ */
 public class ArraysOperations {
 
-    int [][] test= new int[2][2];
 
-
+    /**
+     * this method is to test 2 arrays if they are equal
+     * @param twoDArray taking the test array from the user
+     * @param twoDArray2 taking the test array from the user
+     * @return x which is a boolean function to determain if the 2 arrays are equal
+     */
     public static boolean equality2DArray(int [][] twoDArray, int [][]twoDArray2){
 
         boolean x=true;
@@ -20,4 +33,127 @@ public class ArraysOperations {
         }
         return x;
     }
+
+    /**
+     * takes one array and copy the last values into a 1D array and will present it
+     * @param twoDArray taking the test array from the user
+     * @param new1DArray using this array to store the last 2 values in the 2D array
+     * @return new1DArray which is the resulting array from the copying operation
+     */
+    public static int [] copyTheLastPortion(int [][] twoDArray, int [] new1DArray){
+        for (int i=0;i<twoDArray.length;i++) {     //take input from user
+            for (int j = 0; j < twoDArray.length; j++) {
+
+                new1DArray[i]=twoDArray[i][j];
+            }
+        }
+        return new1DArray;
+    }
+
+    /**
+     * this method takes the first row of the test array and find the sum of them
+     * @param towDArray taking the test array from the user
+     * @return sum which is the sum of the first row
+     */
+    public static int sumFirstRowOfTwoDArray(int [][] towDArray ){
+
+        int sum = 0;
+        for (int i=0; i<towDArray.length-1;i++){
+            for(int j=0; j<towDArray.length;j++){
+                sum=+towDArray[i][j];
+            }
+        }
+        return sum;
+    }
+    /**
+     * this method takes the second row of the test array and find the sum of them
+     * @param towDArray taking the test array from the user
+     * @return sum which is the sum of the second row
+     */
+    public static int sumSecondRowOfTwoDArray(int [][] towDArray ){
+
+        int sum = 0;
+        for (int i=1; i<towDArray.length;i++){
+            for(int j=0; j<towDArray.length;j++){
+                sum=+towDArray[i][j];
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * finds the hamming distance of the passed 2 arrays
+     * @param towDArray taking the test array from the user
+     * @param twoDArray2 taking the test array from the user
+     * @return ham which is the hamming distance and it is an integer
+     */
+    public static int hammingDistanceOfTwoArrays(int [][] towDArray, int[][] twoDArray2){
+
+        int ham = 0;
+        for (int i=1; i<towDArray.length;i++){
+            for(int j=0; j<towDArray.length;j++){
+                if (towDArray[i][j]!=twoDArray2[i][j])
+                ham++;
+            }
+        }
+        return ham;
+    }
+
+    /**
+     * this method uses takes the 2D array and convert it into 1D array.
+     * And it uses the bubble sort algorithm to sort the resulting array from the smallest
+     * to largest. the way it works is by finding if the value is larger than the next one
+     * and do a swap if that was the case. then increase the index byu one until the end of the sort
+     * @param twoDArray taking the test array from the user
+     * @return temp which is the sorted array
+     */
+    public static int [] sortedArray(int [][]twoDArray){
+        int []temp=new int[4];
+
+        for (int i=0,count=0; i<twoDArray.length;i++){
+            for(int j=0; j<twoDArray.length;j++){
+                temp[count]=twoDArray[i][j];
+                count++;
+            }
+        }
+        //I could have put twoDArray.length - 1 to make it more effecient than hard coding the value but
+        // I'm too tired to do that now hahahahah
+        for (int i=0;i<3;i++){
+                for (int j=0;j<3-i;j++){
+                    if (temp[j] > temp[j+1]) {
+                        int index=temp[j];
+                        temp[j]=temp[j+1];
+                        temp[j+1]=index;
+                    }
+                    }
+            }
+        return temp;
+    }
+
+    /**
+     * this method do the binary search algorithem to find the key value given by the user
+     * @param oneDArray the sorted array from before
+     * @param x the key given by the user
+     * @param min the smallest index of the array
+     * @param max the largest index of the array
+     * @return found which is a boolean value that tells if the key was found or not
+     */
+    public static boolean binarySearch(int[] oneDArray, int x,int min, int max) {
+        boolean found = false;
+        while (min <= max) {
+            int mid = min+((max - min)/2);
+            if (oneDArray[mid] == x){
+                found = true;
+                break;
+            }
+            else if (oneDArray[mid] < x){
+                min = mid+1;
+            }
+            else if (oneDArray[mid] > x){
+                max = mid-1;
+            }
+        }
+        return found;
+    }
 }
+
--- src/Assign_01/Test.java
+++ src/Assign_01/Test.java
@@ -5,29 +5,57 @@ import java.util.Scanner;
 public class Test extends ArraysOperations {
     /**
      *the main function is to get the output of each function
-     * and to test
+     * and to test the methods inherated from the other class
      * @param args; the main parameter which passes the arguments
      */
     public static void main(String[] args) {
 
         int [][] test= new int[2][2];
         int [][] test2= new int[2][2];
+        int [] temp= new int [2];
         Scanner input = new Scanner(System.in);
-        for (int i=0;i<test.length;i++){     //print out 2 D array
-            for (int j=0;j<test.length;j++){
-                System.out.println("please enter the"+(i+1)+"of the first 2D array");
-                test[i]=input.nextInt();
 
-
-        System.out.println("are the 2D arrays equal?");
-        System.out.println(equality2DArray(test, test2));
-        System.out.println("\n");
-        System.out.print("the sum of the 1D array is = \n");
-
-        for (int i=0;i<result.length;i++){     //print out 2 D array
-            for (int j=0;j<result.length;j++){
-                System.out.print(result[i][j] + " ");
+        for (int i=0;i<test.length;i++) {     //take input from user
+            for (int j = 0; j < test.length; j++) {
+                System.out.println("please enter the " + (i + 1) + " of the first 2D array\n");
+                test[i][j]=input.nextInt();
             }
         }
+        for (int i=0;i<test2.length;i++) {     //take input from user
+            for (int j = 0; j < test2.length; j++) {
+                System.out.println("please enter the " + (i + 1) + " of the second 2D array\n");
+                test2[i][j]=input.nextInt();
+            }
+        }
+        //input.close();
+
+        System.out.println("are the 2 arrays equal to each other?\n");
+        System.out.println(equality2DArray(test,test2)+"\n");
+
+        System.out.println("\n");
+        System.out.println("we will print the last 2 values of the columns of the first array you put in oneD array \n ");
+
+        temp=copyTheLastPortion(test,temp);
+
+        for (int i=0; i<temp.length;i++){
+            System.out.println(temp[i]+"\s");
+        }
+        System.out.println("\n");
+        System.out.println("the sum of the first row of the first array is \n ");
+        System.out.println(sumFirstRowOfTwoDArray(test)+"\n");
+        System.out.println("the sum of the second row of the first array is \n ");
+        System.out.println(sumSecondRowOfTwoDArray(test)+"\n");
+        System.out.println("the hamming distance between the 2 arrays is \n");
+        System.out.println(hammingDistanceOfTwoArrays(test,test2)+"\n");
+        System.out.println("the sorted array after putting it in 1D array is \n");
+        int []sorted=sortedArray(test);
+        for (int i=0; i<4;i++){
+            System.out.print(sorted[i]+" ");
+        }
+        System.out.println("\n");
+        System.out.println("please enter what are you searching for \n");
+        int key=input.nextInt();
+        System.out.print("the key that you have been searching for found to be: \n");
+        System.out.print(binarySearch(sorted,key,0, sorted.length));
     }
 }
